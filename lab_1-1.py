0157e2b1082fa0629e3ec5363a3a93ddf8bd0251
Darick <p21dalteus@fairfieldprep.org>
2020-12-03 10:30:40 -0500

    lab

---
+++ lab_1-1.py
@@ -0,0 +1,9 @@
+# Author: DA (AMDG) 12/3/2020
+
+def greeting():
+    '''Prints "Hellp World!"'''
+    print("Hello World!")
+
+
+help(greeting)
+greeting()
---
+++ lab_1-2.py
@@ -0,0 +1,9 @@
+# Author: DA (AMDG) 12/3/2020
+
+def find_sum(num1, num2):
+    num_sum = num1 + num2
+    return num_sum
+
+
+my_sum = find_sum(111, 222)
+print(my_sum)
